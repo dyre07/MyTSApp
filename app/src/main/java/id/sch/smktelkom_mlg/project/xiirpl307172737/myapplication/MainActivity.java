81d41b99f036b69516f3392709b38c9f7eb51f79
Widya Restiani <widya_restiani_23rpl@student.smktelkom-mlg.sch.id>
2016-11-12 21:01:58 +0700

    Activity Login

--- app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl307172737/myapplication/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl307172737/myapplication/MainActivity.java
@@ -1,7 +1,9 @@
 package id.sch.smktelkom_mlg.project.xiirpl307172737.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.buttonMurid).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, MainMenuActivity.class));
+            }
+        });
+
+        findViewById(R.id.buttonGuru).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+            }
+        });
     }
 }
--- app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl307172737/myapplication/MainMenuActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project/xiirpl307172737/myapplication/MainMenuActivity.java
@@ -1,13 +1,32 @@
 package id.sch.smktelkom_mlg.project.xiirpl307172737.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import static id.sch.smktelkom_mlg.project.xiirpl307172737.myapplication.R.id.textView;
 
 public class MainMenuActivity extends AppCompatActivity {
 
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
+
+        findViewById(R.id.imageViewJadwal).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainMenuActivity.this, ScheduleActivity.class));
+            }
+        });
+
+        textView = (TextView) findViewById(R.id.textViewUsername);
+
+        Intent intent = getIntent();
+
+        textView.setText("Welcome User " + intent.getStringExtra(LoginActivity.KEY_USERNAME));
     }
 }
